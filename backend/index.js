20282c45804aface9502cc4ef3cf20d1cf2ae7ff
Nhan Phan <nio.phan@tuni.fi>
2026-01-01 00:25:55 +0200

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
 
 let notes = [
@@ -28,7 +28,7 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 app.use(express.static('dist'))
