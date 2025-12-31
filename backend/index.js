95c89c28805f1f378f577f163fc3505951db2648
Nhan Phan <nio.phan@tuni.fi>
2025-12-31 20:02:38 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GETi and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello 22World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+  const myHeader = request.get('Content-Type'); // case insensitive
+  console.log('My-Header value:', myHeader);
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.statusMessage = "Current page does not work";
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: body.important || false
+  }
+
+  notes = notes.concat(note)
+  response.json(note)
+
+  const myHeader = request.get('Content-Type'); // case insensitive
+  console.log('My-Header value:', myHeader);
+  console.log(request.headers)
+}) 
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3002
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
